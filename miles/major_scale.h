171eb12c6dace2376a0ad82134c5e8b7677233fc
Joe Turner <joe@oampo.co.uk>
2013-03-18 15:42:51 +0000

    Add tuning argument to major scale

--- miles/major_scale.h
+++ miles/major_scale.h
@@ -2,11 +2,12 @@
 #define MILES_MAJOR_SCALE_H
 
 #include "scale.h"
+#include "equal_temperament_tuning.h"
 
 namespace miles {
 
-Scale MajorScale() {
-    return Scale({0u, 2u, 4u, 5u, 7u, 9u, 11u});
+Scale MajorScale(Tuning tuning=EqualTemperamentTuning(12)) {
+    return Scale({0u, 2u, 4u, 5u, 7u, 9u, 11u}, tuning);
 }
 
 }
